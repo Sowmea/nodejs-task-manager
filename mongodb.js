dca3c9c03d1f97b5f2cc8671d53cf1f0d294328b
sowmahe993@gmail.com <sowmahe993@gmail.com>
2021-07-20 16:59:48 +0800

    Update one update many

--- mongodb.js
+++ mongodb.js
@@ -15,45 +15,42 @@ MongoClient.connect(
       return console.log('Unable to connect to database');
     }
 
-    // const db = client.db(databaseName);
-    // db.collection('Users').findOne(
-    //   {
-    //     _id: new ObjectId('60f647d128bfd82ea1f90ed6')
-    //   },
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log('Unable to fetch');
-    //     }
-    //     console.log(user);
-    //   }
-    // );
-
-    // db.collection('Users')
-    //   .find({
-    //     age: 1
-    //   })
-    //   .toArray((error, users) => {
-    //     console.log(users);
-    //   });
-
-    // db.collection('Users')
-    //   .find({
-    //     age: 1
-    //   })
-    //   .count((error, count) => {
-    //     console.log(count);
-    //   });
-
     const db = client.db(databaseName);
-    db.collection('Tasks')
-      .find({
-        completed: true
-      })
-      .toArray((error, task) => {
-        if (error) {
-          return console.log('Unable to fetch');
-        }
-        console.log(task);
-      });
+
+    // db.collection('Users')
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectId('60f64be13b7ae603ad8dffe3')
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 1
+    //       }
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+
+    // db.collection('Tasks')
+    //   .updateMany(
+    //     {
+    //       completed: false
+    //     },
+    //     {
+    //       $set: {
+    //         completed: true
+    //       }
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
   }
 );
