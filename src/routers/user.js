ef7387f26b5c21263b6139b0b5d0ddc400815064
sowmahe993@gmail.com <sowmahe993@gmail.com>
2021-07-29 17:03:35 +0800

    Add auth to other user endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -59,21 +59,7 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user);
 });
 
-router.get('/users/:id', async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body);
 
   const allowedUpdates = ['name', 'email', 'password', 'age'];
@@ -86,15 +72,11 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id);
+    const user = req.user;
 
     updates.forEach(update => (user[update] = req.body[update]));
 
     await user.save();
-
-    if (!user) {
-      return res.status(400).send();
-    }
 
     return res.send(user);
   } catch (error) {
@@ -102,13 +84,10 @@ router.patch('/users/:id', async (req, res) => {
   }
 });
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send({ Error: 'No user found' });
-    }
-    res.send(user);
+    await req.user.remove();
+    res.send(req.user);
   } catch (error) {
     res.status(400).send(error);
   }
